a35839442436ac761659217fc4bc8aea22854b13
Damian Sadowski <damiansadowskidev@gmail.com>
2026-04-06 21:35:09 +0200

    Dodano pobieranie zmiennych do logowania z ssm

--- terraform/UserDataScripts/userDataEC2.sh
+++ terraform/UserDataScripts/userDataEC2.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # System updates
-sudo dnf update
-sudo dnf upgrade
+sudo dnf update -y
 
 # Install docker
 sudo dnf install -y docker
@@ -10,4 +9,12 @@ sudo dnf install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Retrieve parameters for further use in logging into GCR.IO and store them in env variable
+GHCR_LOGIN=$(aws ssm get-parameter --name "/prod/ghcr-login" --query "Parameter.Value" --output text --with-decryption)
+export GHCR_LOGIN
 
+GHCR_PASSWORD=$(aws ssm get-parameter --name "/prod/ghcr-password" --query "Parameter.Value" --output text --with-decryption)
+export GHCR_PASSWORD
+
+# Login into GHCR 
+echo "$GHCR_PASSWORD" | docker login ghcr.io -u "$GHCR_LOGIN" --password-stdin
