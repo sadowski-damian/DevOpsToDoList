f9f264a4cbab380ecf12cde13e9563efb8f6696a
Damian Sadowski <damiansadowskidev@gmail.com>
2026-04-06 21:11:04 +0200

    Prace nad skryptem userdata dla ec2 w asg

--- terraform/UserDataScripts/userDataEC2.sh
+++ terraform/UserDataScripts/userDataEC2.sh
@@ -1,1 +1,13 @@
-# Test
+#!/usr/bin/env bash
+# System updates
+sudo dnf update
+sudo dnf upgrade
+
+# Install docker
+sudo dnf install -y docker
+
+# Run and make Docker run on every system start
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
