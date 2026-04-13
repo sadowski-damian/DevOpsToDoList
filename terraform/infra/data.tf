812e799135dadd0bdfb0119c25e948494daf55ef
Damian Sadowski <damiansadowskidev@gmail.com>
2026-04-13 18:04:27 +0200

    Naprawa dlugiego czasu ladowania planu dla infra (prawdopodobna przyczyna to filtr w dacie)

--- terraform/infra/data.tf
+++ terraform/infra/data.tf
@@ -31,14 +31,8 @@ data "aws_iam_policy_document" "monitoring_ec2_discovery" {
   }
 }
 
-data "aws_ami" "amazon_linux_2023" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*x86_64"]
-  }
+data "aws_ssm_parameter" "al2023_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
 
 data "terraform_remote_state" "network" {
