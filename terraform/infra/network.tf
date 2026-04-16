1c62198fe9433cdb13f43913e26857da00b5110a
Damian Sadowski <damiansadowskidev@gmail.com>
2026-04-16 14:40:03 +0200

    Dodano route53 zone, certyfikat acm oraz rekord wskazujacy na naszego lb

--- terraform/infra/network.tf
+++ terraform/infra/network.tf
@@ -103,16 +103,45 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 # Create listener for alb
 # Listener catches incoming traffic from the internet and forwards it to our target group
-# HTTP is used because no SSL certificate is configured for this learning project
+# We ve got SSL now
 #tfsec:ignore:aws-elb-http-not-used
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = data.terraform_remote_state.network.outputs.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.main_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_route53_record" "app" {
+  zone_id = data.terraform_remote_state.network.outputs.route_53_zone_id
+  name    = "wenttoprod.damiansadowski.cloud"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main_alb.dns_name
+    zone_id                = aws_lb.main_alb.zone_id
+    evaluate_target_health = true
   }
 }
 
